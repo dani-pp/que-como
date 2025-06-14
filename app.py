f70d25d15dd50c59c10a90ed7e65c2fa9636e0dc
dani-pp <pesodaniza@gmail.com>
2025-06-14 00:34:42 -0400

    añadidas redirecciones

--- app.py
+++ app.py
@@ -11,16 +11,31 @@ db=client.get_default_database()
 usuarios = db['usuarios']
 recetas = db['recetas']
 
-@app.route('/')
-@app.route('/')
-def inicio():
-    todas_las_recetas = list(recetas.find())
-    print("Recetas encontradas:", todas_las_recetas)  # 👈 agrega esto
-    return render_template("index.html", recetas=todas_las_recetas)
+#@app.route('/')
+#def inicio():
+#   todas_las_recetas = list(recetas.find())
+#    print("Recetas encontradas:", todas_las_recetas)  # 👈 agrega esto
+#    return render_template("index.html", recetas=todas_las_recetas)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/iniciar-sesion')
+def iniciarSesion():
+    return render_template('iniciarSesion.html')
+
+@app.route('/recetas')
+def verRecetas():
+    return render_template('recetas.html')
+
+@app.route('/FAQ')
+def faq():
+    return render_template('faq.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
