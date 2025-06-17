e3435e73d73054a6f21f635b4d5ce9586015b715
dani-pp <pesodaniza@gmail.com>
2025-06-16 21:59:24 -0400

    blueprint de iniciar sesion

--- routes/iniciarSesion.py
+++ routes/iniciarSesion.py
@@ -1,4 +1,12 @@
-from flask import Flask, request
-import app.py
+from flask import Flask, render_template, request, Blueprint
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-@app.route('/procesar')
+iniciarSesion_route = Blueprint('iniciarSesion_route', __name__)
+
+@iniciarSesion_route.route('/validar-usuario')
+def validarUsuario():
+  correo = request.form.get('inputEmail')
+  password = request.form.get('inputPass')
